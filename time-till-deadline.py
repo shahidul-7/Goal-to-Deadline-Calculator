4ff6984be325e339a4d912ef91ebedf7064655c5
shahidul-7 <shahidul.du34@gmail.com>
2024-07-10 14:21:19 +0600

    First Commit

---
+++ time-till-deadline.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+user_input = input("Please input your Goal and deadline(like: Learn Python:01.05-2025) and it will show how many days are left!: ")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+
+print(f"Your goal is: {goal} and deadline is: {deadline}")
+
+deadline_date = datetime.strptime(deadline, '%d.%m.%Y') 
+today_date = datetime.today()
+
+final_result = deadline_date - today_date
+
+print(f"You have {final_result.days} left to fulfill your goal, Hurry up!!")
+
+print("Thanks for using our app!")
